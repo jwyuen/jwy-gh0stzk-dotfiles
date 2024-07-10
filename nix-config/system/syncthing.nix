c63d139c845797ce36a8585063bd6416ed5847d1
jwy <jwingy@gmail.com>
2024-07-10 14:35:27 -0400

    Opened ports in firewall for syncthing

--- nix-config/system/syncthing.nix
+++ nix-config/system/syncthing.nix
@@ -10,4 +10,11 @@ lib.mkIf (syncthing == true) {
     configDir = "/home/${username}/.config/syncthing";   # Folder for Syncthing's settings and keys
     };
   };
+
+  # Syncthing ports: 8384 for remote access to GUI
+  # 22000 TCP and/or UDP for sync traffic
+  # 21027/UDP for discovery
+  # source: https://docs.syncthing.net/users/firewall.html
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }
